c5795b3540002ae483b6d2e06b23914e68247f4f
meows <b5c6@protonmail.com>
2020-01-10 09:08:17 -0600

    fix(index.js): refactor and simplify usage
    - Only one value is processed at a time.
    - Input can be read as agrument, via stdin pipe, or as tty stdin
    line-by-line
    - Output is standardized and simplified.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- index.js
+++ index.js
@@ -5,24 +5,25 @@ const meow = require('meow');
 require('process');
 var path = require('path');
 var chalk = require('chalk');
-const ser = require('@etclabscore/eserialize')
+const ser = require('@etclabscore/eserialize');
 
 const cli = meow(`
+    The eserialize CLI reads from stdin and either serializes or deserializes input.
+
     Usage
-      $ eserialize <input> <input> ...
+      $ eserialize <input>
 
     Examples
 
-    $ eserialize-cli 0xdeadbeef 42
-                     |
-                     0x:Number 0xdeadbeef 3735928559
-                     0x:String 0x30786465616462656566 ޭ��
-                     0x:Date   0xNaN 2088-05-20T21:55:59.000Z
+    $ eserialize-cli 0xdeadbeef
 
-                                |
-                                0x:Number 0x2a 66
-                                0x:String 0x3432
-                                0x:Date   0x876e8b60 1970-01-01T00:01:06.000Z
+    $ eserialize-cli "my special note"
+
+    $ echo 42 | eserialize-cli
+
+    $ eserialize-cli
+    42
+    (number) = 0x2a
 `, {
     flags: {
         nocolor: {
@@ -32,83 +33,81 @@ const cli = meow(`
     }
 });
 
-var basenameLen = function() {
-    return path.basename(process.argv[1]).length;
+var regHex = /^(0x|00)[a-f0-9]+$/im;
+var regNumber = /^\d+$/m;
+
+var isSerialializedFormat = function(input) {
+    return (regHex.test(input));
 };
 
-var offset = function(i) {
-    return " ".repeat(i);
+var stdOutPrettyKeyValue = function(k, v) {
+    if (!cli.flags.nocolor) {
+        console.log(chalk.magenta(k), "=", chalk.cyan(v));
+    } else {
+        console.log(k, "=", v);
+    }
 };
 
-var formatConversionLabel = function(label) {
-    if ( cli.flags.nocolor) {
-        return label;
-    }
-    var ls = label.split(":");
-    var ls1 = chalk.magenta(ls[0]);
-    var ls2 = chalk.cyan(ls[1]);
-    return ls1 + chalk.gray(":") + ls2;
-}
+var deserialize = function(input) {
+    var num = ser.hexToNumber(input);
+    stdOutPrettyKeyValue("number", num);
 
-function formatToHex(input, output) {
-    if ( cli.flags.nocolor ) {
-        return output;
-    }
-    if (input === output) {
-        return chalk.underline(input);
-    }
-    return chalk.magenta(output);
-}
+    var str = ser.hexToString(input);
+    stdOutPrettyKeyValue("string", str);
 
-function formatToVal(input, output) {
-    if ( cli.flags.nocolor ) {
-        return output;
-    }
-    return chalk.cyan(output);
-}
-
-var handleInputHex = function(o, input) {
-    o += 2; // space + index0
-    console.log();
-    console.log(offset(o), formatConversionLabel("0x:Number"), formatToHex(input, ser.numberToHex(+input)), formatToVal(input, ser.hexToNumber(input) ));
-    console.log(offset(o), formatConversionLabel("0x:String"), formatToHex(input, ser.stringToHex(input)), formatToVal(input, ser.hexToString(input) ));
-
-    // FIXME
-    // Date (HexTo) throws 'Invalid time value' sometimes.
-    // Reproduce with 0x74657374696e67737472696e67
-    var d = "";
-    var outputd = new Date();
-    var gotd;
-    try {
-        d = new Date(input) || true;
-    } catch (err) {
-        // noop
-    }
-    try {
-        gotd = ser.hexToDate(input);
-    } catch {
-        // noop
-    }
-    if (typeof gotd != undefined) {
-        gotd = new Date();
-    }
-    console.log(offset(o), formatConversionLabel("0x:Date  "), formatToHex(input, ser.dateToHex(d)), formatToVal(input, outputd.toISOString() ));
-}
-
-var foo = function(input, flags) {
-    var len = basenameLen();
-    for (var i = 0; i < Object.keys( cli.flags ).length; i++) {
-        if (cli.flags[Object.keys(cli.flags)[i]]) {
-            len += 3;
-        }
-    }
-    for (var i = 0; i < input.length; i++) {
-        if (i > 0) {
-            len += input[i - 1].length + 1;
-        }
-        handleInputHex(len, input[i]);
-    }
-    console.log();
+    var dat = ser.hexToDate(input);
+    stdOutPrettyKeyValue("date", dat);
 };
 
-foo(cli.input, cli.flags);
+var isDateFormat = function(input) {
+    var d = new Date(input);
+    return d instanceof Date && !isNaN(d.valueOf());
+};
+
+var handleInput = function(input) {
+
+    // Empty is noop, eg EOF, newlines.
+    if (input.length == 0) {
+        return;
+    }
+
+    // If appears serialized, deserialize it.
+    if (isSerialializedFormat(input)) {
+        deserialize(input);
+        return;
+    }
+
+    // If unserialized, serialize it.
+
+    // Number.
+    if (regNumber.test(input)) {
+        stdOutPrettyKeyValue("(number)", ser.numberToHex(+input));
+        return;
+    }
+
+    // Date.
+    if (isDateFormat(input)) {
+        var d = new Date(input);
+        stdOutPrettyKeyValue("(date)", ser.dateToHex(d));
+        return;
+    }
+
+    // String.
+    stdOutPrettyKeyValue("(string)", ser.stringToHex(input));
+};
+
+var run = function(input, flags) {
+
+    // Args were passed.
+    if (input.length > 0) {
+        handleInput(input[0]);
+        return;
+    }
+
+    // No args were passed; read from stdin.
+    // This will either read line-by-line from TTY stdin in an interactive way,
+    // or, if stdin is piped, it will read that and then exit.
+    process.stdin.pipe(require('split')()).on('data', handleInput);
+};
+
+run(cli.input, cli.flags);
